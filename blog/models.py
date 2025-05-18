a85c0550aa580f3042e6c5a9c04be8f28276b858
Emad <sudani.emad@gmail.com>
2025-05-18 20:41:02 +0330

    feat: init post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(
+        max_length=250,
+    )
+    slug = models.SlugField(
+        max_length=250,
+        allow_unicode=True,
+    )
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
