5eae9e1de3b897ad946438a82f26edc0a821f245
Emad <sudani.emad@gmail.com>
2025-06-08 19:33:21 +0330

    feat: add most comments posts tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from ..models import Post
+from django.db.models import Count
 
 register = template.Library()
 
@@ -13,3 +14,9 @@ def show_latest_posts(count=3):
     return {
         'latest_posts': latest_posts,
     }
+
+@register.simple_tag
+def get_most_commented_posts(count=3):
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
